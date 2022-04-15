b9b86c26dfcd32698d956c8b740c87d73317e49c
Michael Fenske <michael@codertrix.de>
2022-04-15 21:14:08 +0200

    Add Day 23 (2021): Amphipod

---
+++ 2021/src/bin/day23/day23.rs
@@ -0,0 +1,32 @@
+// ---------------------------------------------------
+//  Advent of Code 2021
+//  Day 23: Amphipod
+//
+//  Copyright © 2021 Michael Fenske. See LICENSE.txt.
+// ---------------------------------------------------
+
+pub fn run() {
+    println!("Result for puzzle 1: {}", puzzle_one());
+    println!("Result for puzzle 2: {}", puzzle_two());
+
+    println!("\nI solved the puzzles of this day manually, without coding.");
+    println!("See the files 'puzzle_one.txt' and 'puzzle_two.txt'");
+}
+
+
+// ----------
+//  Puzzle 1
+// ----------
+
+fn puzzle_one() -> usize {
+    15338
+}
+
+
+// ----------
+//  Puzzle 2
+// ----------
+
+fn puzzle_two() -> usize {
+    47064
+}
